c729b58aaf29e8a971fb3f61c841d1e36c024602
Edvard Fagerholm <edvard.fagerholm@gmail.com>
2018-02-18 14:15:27 +0200

    Add prettier + run it on repo

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const App = () => (
-  <div className="app" />
-);
+const App = () => <div className="app" />;
 
 render(<App />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  entry: ["./js/ClientApp.jsx"],
+  entry: ['./js/ClientApp.jsx'],
   output: {
-    path: path.resolve(__dirname, "public"),
-    filename: "bundle.js",
-    publicPath: '/public/' 
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/public/'
   },
   stats: {
     colors: true,
@@ -14,16 +14,16 @@ module.exports = {
   },
   module: {
     rules: [
-       {
-         test: /\.(js|jsx)$/,
-         exclude: /node_modules/,
-         use: ['babel-loader']
-       },
-       {
-         test: /\.js$/,
-         exclude: /node_modules/,
-         use: ['babel-loader', 'eslint-loader']
-       }
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader']
+      }
     ]
   }
 };
