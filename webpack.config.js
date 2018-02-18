44b463be9e2a72eb0d4a1aec51b0911a6564527c
Edvard Fagerholm <edvard.fagerholm@gmail.com>
2018-02-18 14:07:26 +0200

    First set of files. Might still have issues.[C

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = {
+  entry: ["./js/ClientApp.jsx"],
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+    publicPath: '/public/' 
+  },
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: false
+  },
+  module: {
+    rules: [
+       {
+         test: /\.(js|jsx)$/,
+         exclude: /node_modules/,
+         use: ['babel-loader']
+       },
+       {
+         test: /\.js$/,
+         exclude: /node_modules/,
+         use: ['babel-loader', 'eslint-loader']
+       }
+    ]
+  }
+};
