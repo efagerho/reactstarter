7f081c22e547e60f770c6c17b81437f84518f684
Edvard Fagerholm <edvard.fagerholm@gmail.com>
2018-02-19 00:00:42 +0200

    Add BrowserRouter to default project

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Landing from './Landing/Landing';
 
-const App = () => <div className="app" />;
+const App = () => (
+  <BrowserRouter>
+    <div className="app">
+      <Switch>
+        <Route exact path="/" component={props => <Landing {...props} />} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
 
 render(<App />, document.getElementById('app'));
---
+++ js/Landing/Landing.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Landing = props => <p>Hello World</p>;
+
+export default Landing;
